4ff262de25259bda74356cb22e483e9eb438637e
wavis421 <wendy.avis@jointheleague.org>
2016-11-22 12:33:59 -0800

    Add MeetMyFamily class & runner.

--- src/MeetMyFamily.java
+++ src/MeetMyFamily.java
@@ -1,19 +1,22 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class MeetMyFamily {
-	ArrayList<MyFamily> famList = new ArrayList<MyFamily>();
+	ArrayList<FamilyMember> famList = new ArrayList<FamilyMember>();
 
 	public MeetMyFamily() {
 	}
 	
-	public void addFamilyMember (String newMember) {
-		famList.add(new MyFamily(newMember));
+	public void addFamilyMember (String newMember, int age) {
+		famList.add(new FamilyMember(newMember, age));
 	}
 	
 	public void showFamilyMembers () {
+		Collections.sort(famList);
+		
 		System.out.println("My family members are: ");
-		for (MyFamily f : famList) {
-			System.out.println("   " + f.getName());
+		for (FamilyMember f : famList) {
+			System.out.println("   " + f.getName() + ", age " + f.getAge());
 		}
 	}
 }
--- src/MeetMyFamilyRunner.java
+++ src/MeetMyFamilyRunner.java
@@ -3,9 +3,11 @@ public class MeetMyFamilyRunner {
 	public static void main(String[] args) {
 		MeetMyFamily fam = new MeetMyFamily();
 		
-		fam.addFamilyMember("Wendy");
-		fam.addFamilyMember("Natasha");
-		fam.addFamilyMember("Jacqueline");
+		fam.addFamilyMember("Wendy", 58);
+		fam.addFamilyMember("Natasha", 28);
+		fam.addFamilyMember("Jacqueline", 24);
+		fam.addFamilyMember("Kristy", 25);
+		fam.addFamilyMember("Graham", 58);
 		
 		fam.showFamilyMembers();
 	}
